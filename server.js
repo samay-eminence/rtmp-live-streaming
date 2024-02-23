dd8d2ca1dfe6e80949c0d32f9fdfff49f05d6020
Samay Nandan <samay.emience@gmail.com>
2024-02-23 15:04:54 +0530

    Node media server added

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
-const express = require("express");
 const NodeMediaServer = require("node-media-server");
-
-const app = express();
-const port = 3000;
 
 const config = {
   rtmp: {
@@ -22,11 +18,3 @@ const config = {
 const nms = new NodeMediaServer(config);
 
 nms.run();
-
-app.get("/", (req, res) => {
-  res.send("Live streaming server is running...");
-});
-
-app.listen(port, () => {
-  console.log(`Express server listening at http://localhost:${port}`);
-});
